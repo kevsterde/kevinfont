2c972b64d546bdcd4d8f9ea99c55b77a715716aa
SENSlE <kevinwebsterr2016@gmail.com>
2023-07-13 00:56:15 +0800

    update responsive

--- src/components/FontContainer.jsx
+++ src/components/FontContainer.jsx
@@ -9,8 +9,9 @@ function FontContainer(props) {
 
 
   return (
-    <div onClick={() => UpdateClick(props.font)} className="font w-[20%] flex justify-center items-center flex-col text-center p-10 bg-color4 hover:bg-color2 hover:text-white cursor-pointer rounded-[10px]  text-color2">
-        <h2 className="text-4xl">{props.font.Title}</h2>
+    <div onClick={() => UpdateClick(props.font)}
+     className="font w-[100%] sm:w-[46%] md:w-[29%] lg:w-[20%] flex justify-center items-center flex-col text-center p-10 bg-color4 hover:bg-color2 hover:text-white cursor-pointer rounded-[10px]  text-color2">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl">{props.font.Title}</h2>
     </div>
 
 
