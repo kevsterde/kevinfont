479131b58796024ba1b225c37a0a26fd564a8720
SENSlE <kevinwebsterr2016@gmail.com>
2023-07-15 08:54:53 +0800

    update logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-
+import logo from '../images/icon.png'
 function Header(props) {
     return (
         <header className='spacer bg-color3 text-black '>
             <div className="wrapper">
-                <div className="header_con flex justify-between py-5">
-
-                    <h1 className='text-4xl font-bold'>FONTS</h1>
-                    <button onClick={() => {props.setShowPopUp(!props.showPopUp)}} className='btnAdd bg-color1 text-white font-semibold px-10 rounded-[150px] hover:bg-color2'>Add +</button>
+                <div className="header_con flex justify-between py-5 items-center">
+                    <a href="/" className='rounded-[20px] overflow-hidden'><img src={logo}/></a>
+                    <button onClick={() => {props.setShowPopUp(!props.showPopUp)}} className='btnAdd bg-color1 text-white font-semibold px-10 py-4 rounded-[150px] hover:bg-color2'>Add +</button>
                 </div>
             </div>
         </header>
